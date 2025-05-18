1f3085fb52d6219d1576e64d22d416eff1ea234b
morison <frederickmorison@gmail.com>
2025-05-18 10:42:55 +0100

    initial commit

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+
+app.use(cors());
+app.get("/*splat", async (req: Request, res: Response) => {
+  try {
+    console.log("get request recieved");
+    const url = req.originalUrl.slice(1);
+    const response = await axios.get(url);
+    res.status(200).send(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
