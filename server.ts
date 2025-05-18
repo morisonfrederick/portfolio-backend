5d56aa89149ac353b0691c03b3a80b2dc74ddab3
morison <frederickmorison@gmail.com>
2025-05-18 11:06:19 +0100

    fix url issue

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ app.get("/*splat", async (req: Request, res: Response) => {
     console.log("get request recieved");
     const url = req.originalUrl.slice(1);
     console.log(url);
-    const response = await axios.get(url);
+    const response = await axios.get(`https://${url}`);
     res.status(200).send(response.data);
   } catch (error) {
     console.log(error);
